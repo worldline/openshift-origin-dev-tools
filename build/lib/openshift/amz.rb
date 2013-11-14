004d9f3f36d68b8c408e31f09a3eaeb4e6e561e8
Dan McPherson <dmcphers@redhat.com>
2013-11-14 13:14:04 -0500

    reducing the retries

--- build/lib/openshift/amz.rb
+++ build/lib/openshift/amz.rb
@@ -287,12 +287,12 @@ module OpenShift
     end
 
     def repair_ssh(instance, ssh_user='root')
-      output, exit_code = ssh(instance.dns_name, "echo Success", 90, true, 24, ssh_user)
+      output, exit_code = ssh(instance.dns_name, "echo Success", 90, true, 16, ssh_user)
       if exit_code == 255
         puts "Rebooting instance..."
         instance.reboot
         puts "Done"
-        output, exit_code = ssh(instance.dns_name, "echo Success", 90, true, 16, ssh_user)
+        output, exit_code = ssh(instance.dns_name, "echo Success", 90, true, 8, ssh_user)
       end
       return exit_code == 0
     end
